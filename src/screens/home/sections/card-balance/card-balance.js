cef919eb9157b98ac8bc21d109ece0e7434ca5fd
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2022-01-26 10:07:05 +0700

    fix(card-balance): split every replections value

--- src/screens/home/sections/card-balance/card-balance.js
+++ src/screens/home/sections/card-balance/card-balance.js
@@ -12,30 +12,38 @@ export default function CardBalance() {
         <h2 className={styles.h2}>Balance</h2>
         <Row>
           <Col xs={6}>
-            <h5 className={styles.h5}>
-              Your pending <br />
-              reflections
-            </h5>
+            <h5 className={styles.h5}>Your pending</h5>
           </Col>
           <Col xs={6}>
             <p className={styles.p}>
               <strong>$FTM 3.50</strong>
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <h5 className={styles.h5}>reflections</h5>
+          </Col>
+          <Col xs={6}>
             <p className={styles.p}>$ 10.20</p>
           </Col>
         </Row>
         <hr className={styles.hr} />
         <Row>
           <Col xs={6}>
-            <h5 className={styles.h5}>
-              Your claimable <br />
-              reflections
-            </h5>
+            <h5 className={styles.h5}>Your claimable</h5>
           </Col>
           <Col xs={6}>
             <p className={styles.p}>
               <strong>$FTM 13.50</strong>
             </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <h5 className={styles.h5}>reflections</h5>
+          </Col>
+          <Col xs={6}>
             <p className={styles.p}>$ 30.12</p>
           </Col>
         </Row>
@@ -45,15 +53,19 @@ export default function CardBalance() {
         <Card.Footer>
           <Row>
             <Col xs={6}>
-              <h5 className={clsx(styles.h5, styles.textGray)}>
-                Your claimed <br />
-                reflections
-              </h5>
+              <h5 className={clsx(styles.h5, styles.textGray)}>Your claimed</h5>
             </Col>
             <Col xs={6}>
               <p className={styles.p}>
                 <strong className={styles.textGray}>$FTM 27.00</strong>
               </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6}>
+              <h5 className={clsx(styles.h5, styles.textGray)}>reflections</h5>
+            </Col>
+            <Col xs={6}>
               <p className={clsx(styles.p, styles.textGray)}>$ 60.21</p>
             </Col>
           </Row>
