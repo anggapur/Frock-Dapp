be8d35799d7dbefc2fb3884111d4e0bfdaec889f
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2022-01-27 09:43:00 +0700

    fix(calculator): set max value ftm price to 20

--- src/screens/home/sections/calculator/calculator.js
+++ src/screens/home/sections/calculator/calculator.js
@@ -64,7 +64,7 @@ export default function Calculator() {
             type="number"
             symbol={{ label: '$', position: 'start' }}
             minValue={1}
-            maxValue={100}
+            maxValue={20}
             value={calculatorStore.ftmPrice}
             setValue={calculatorStore.setFtmPrice}
           />
