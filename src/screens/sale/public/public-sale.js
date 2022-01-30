ec15f2c72b78c2cb40b69d441529096cc16c08cd
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2022-01-30 15:37:13 +0700

    feat: add card coin raised

--- src/screens/sale/public/public-sale.js
+++ src/screens/sale/public/public-sale.js
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Card from "../../../components/card/card"
 import Countdown from "../../../components/countdown/countdown"
+import CardCoinRaised from "../sections/card-coin-raised/card-coin-raised"
 import "./../sale.scss"
 
 export default function PublicSale() {
@@ -25,32 +26,7 @@ export default function PublicSale() {
       </p>
       <Row>
         <Col lg={7}>
-          <Card lineBottom="light">
-            <div className="sale__card__coin-raised">
-              <div className="sale__card__coin-raised__start-end">
-                <div>
-                  <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5.27179 0C2.36143 0 0 2.375 0 5.30208C0 8.22917 2.36143 10.6094 5.27179 10.6094C8.18214 10.6094 10.5488 8.23438 10.5488 5.30729H5.27179V0Z" fill="white"/>
-                  </svg>
-                  <div>
-                    <h4>Start Time:</h4>
-                    <p>28 Dec. 17:00 UTC</p>
-                  </div>
-                </div>
-                <div>
-                  <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5.27179 0C2.36143 0 0 2.375 0 5.30208C0 8.22917 2.36143 10.6094 5.27179 10.6094C8.18214 10.6094 10.5488 8.23438 10.5488 5.30729H5.27179V0Z" fill="white"/>
-                  </svg>
-                  <div>
-                    <h4>End Time:</h4>
-                    <p>30 Dec. 17:00 UTC</p>
-                  </div>
-                </div>
-              </div>
-              sa
-            </div>
-            Starting $Frock Price
-          </Card>
+          <CardCoinRaised />
         </Col>
         <Col lg={5}>
           <Card>sa</Card>
