147d85c3f12bdb618a3d74c18923f5d6107b55ef
Angga Purnajiwa <anggapurdev@gmail.com>
2022-02-09 10:14:26 +0800

    Time Travel Script

--- packages/solidity/scripts/timeTravel.ts
+++ packages/solidity/scripts/timeTravel.ts
@@ -1,8 +1,26 @@
-import { network } from 'hardhat';
+import { network, ethers } from 'hardhat';
 
+/**
+ * @dev to run this function :  yarn solidity run-local scripts/timeTravel.ts --time [time_in_timestamp]
+ * @dev example :  yarn solidity run-local scripts/timeTravel.ts --time 1644166900
+ * @dev You cannot time travel to time that already passed, only able time travel to future time
+ */
 async function main() {
-  await network.provider.send('evm_setNextBlockTimestamp', [0]);
-  await network.provider.send('evm_mine');
+  console.log("Let's Time Travel")
+  var args = process.argv.slice(2);
+  if(args && args[0] === "--time" && args[1]) {
+    const timeDestination = parseInt(args[1])    
+    const latestBlock = await ethers.provider.getBlock("latest")
+    const latestTimestamp = latestBlock.timestamp
+
+    if(timeDestination > latestTimestamp) {
+      await network.provider.send('evm_setNextBlockTimestamp', [timeDestination]);
+      await network.provider.send('evm_mine');
+      console.log(`You're Time Travel to ${timeDestination}`)
+    } else {
+      console.log(`You Cannot Time Travel to Past Time`)
+    }
+  }
 }
 
 main()
