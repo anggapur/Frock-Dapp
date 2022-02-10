fc3cc7e3b8d7bf97d8e1db4f745b40dc6906fb76
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2022-02-10 21:08:20 +0700

    fix: change balance ui

--- packages/react-app/src/screens/sale/sections/card-balance/card-balance.js
+++ packages/react-app/src/screens/sale/sections/card-balance/card-balance.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
+import frockDarkLogo from '../../../../assets/frock-dark-logo-small.svg';
 import frockLogo from '../../../../assets/frock-logo-small.svg';
 import usdcLogo from '../../../../assets/usdc-logo-small.svg';
 import Card from '../../../../components/card/card';
@@ -14,17 +16,32 @@ export default function CardBalance({
   return (
     <Card className={styles.wrapper}>
       <h2>Balance:</h2>
-      <div>
-        <img src={usdcLogo} alt="USDC logo" />
-        <p>{renderNumberFormatter(usdcBalance)} $USDC</p>
-      </div>
-      <div>
-        <img src={frockLogo} alt="FROCK logo" />
-        <p>
-          {renderNumberFormatter(frockBalance)}{' '}
-          {communitySale ? '$aFROCK' : '$bFROCK'}
-        </p>
-      </div>
+      <Row>
+        <Col>
+          <img src={usdcLogo} alt="USDC logo" />
+          <div>
+            {renderNumberFormatter(usdcBalance)}
+            <p>$USDC</p>
+          </div>
+        </Col>
+        <Col>
+          <img
+            src={frockDarkLogo}
+            alt={`${communitySale ? 'aFROCK' : 'bFROCK'} logo`}
+          />
+          <div>
+            {renderNumberFormatter(frockBalance)}
+            <p>{communitySale ? '$aFROCK' : '$bFROCK'}</p>
+          </div>
+        </Col>
+        <Col>
+          <img src={frockLogo} alt="FROCK logo" />
+          <div>
+            {renderNumberFormatter(frockBalance)}
+            <p>$FROCK</p>
+          </div>
+        </Col>
+      </Row>
     </Card>
   );
 }
