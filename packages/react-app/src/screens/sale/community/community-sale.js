d8870cc7edf6b22667bb20516849b609e6f62f66
Koming Yosua <koming@wagely.app>
2022-02-13 00:16:29 +0800

    Fix error on current cap

--- packages/react-app/src/screens/sale/community/community-sale.js
+++ packages/react-app/src/screens/sale/community/community-sale.js
@@ -81,7 +81,7 @@ export default function CommunitySale() {
       if (provider && accounts) {
         await handleGetUSDC();
         await handleGetNRT();
-        await handleGetFrock();
+        // await handleGetFrock();
 
         await handleGetTotalContribution();
 
@@ -114,10 +114,10 @@ export default function CommunitySale() {
     setNRTBalance(formatUnits(nrtBalanceResult, FROCK_DECIMALS));
   };
 
-  const handleGetFrock = async () => {
-    const frockBalanceResult = await frockContract.balanceOf(accounts[0]);
-    setFrockBalance(formatUnits(frockBalanceResult, FROCK_DECIMALS));
-  };
+  // const handleGetFrock = async () => {
+  //   const frockBalanceResult = await frockContract.balanceOf(accounts[0]);
+  //   setFrockBalance(formatUnits(frockBalanceResult, FROCK_DECIMALS));
+  // };
 
   const handleGetIsRedeemEnabled = async () => {
     const isRedeemEnabledResult = await communityOffering.redeemEnabled();
