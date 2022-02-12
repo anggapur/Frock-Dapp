6dfdbc3dfea915fed6b46f3e03c90e41c89a932a
Koming Yosua <koming@wagely.app>
2022-02-13 00:30:59 +0800

    Change address to add token

--- packages/react-app/src/constants/index.js
+++ packages/react-app/src/constants/index.js
@@ -1,5 +1,8 @@
-import darkFrockSmall from '../assets/frock-dark-logo-small.svg';
-import frockSmall from '../assets/frock-logo-small.svg';
+import {
+  COMMUNITY_OFFERING_NRT_ADDR,
+  FAIR_PRICE_NRT_ADDR,
+  FROCK_ADDR,
+} from '@project/contracts/src/address';
 
 export const DAYS_IN_YEAR = 365;
 export const FROCK_SUPPLY = 1000000;
@@ -15,19 +18,19 @@ export const FANTOM_CHAIN_PARAMS = {
 export const USDC_DECIMALS = 6;
 export const FROCK_DECIMALS = 9;
 export const AFROCK_TOKEN_DATA = {
-  address: '0xB0D4afd8879eD9F52b28595d31B441D079B2Ca07',
+  address: COMMUNITY_OFFERING_NRT_ADDR,
   symbol: 'aFROCK',
   decimals: 9,
   image: 'https://fractionalrocket.money/afrock-logo.png',
 };
 export const BFROCK_TOKEN_DATA = {
-  address: '0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB',
+  address: FAIR_PRICE_NRT_ADDR,
   symbol: 'bFROCK',
   decimals: 9,
   image: 'https://fractionalrocket.money/bfrock-logo.png',
 };
 export const FROCK_TOKEN_DATA = {
-  address: '0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d',
+  address: FROCK_ADDR,
   symbol: 'FROCK',
   decimals: 9,
   image: 'https://fractionalrocket.money/frock-logo.png',
