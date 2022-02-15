118102224c978a5042afdb8f5e24a40a955575d6
Robbert Deusing <rj@erunner.nl>
2022-02-15 22:07:53 +0800

    re-enabled public sale tab and rounte

--- packages/react-app/src/app.js
+++ packages/react-app/src/app.js
@@ -5,7 +5,7 @@ import Layout from './components/layout/layout';
 import SuspenseLoading from './components/suspense-loading/suspense-loading';
 
 const Home = lazy(() => import('./screens/home/home'));
-// const PublicSale = lazy(() => import('./screens/sale/public/public-sale'));
+const PublicSale = lazy(() => import('./screens/sale/public/public-sale'));
 const CommunitySale = lazy(() =>
   import('./screens/sale/community/community-sale'),
 );
@@ -16,7 +16,7 @@ export default function App() {
       <Suspense fallback={<SuspenseLoading />}>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="public-sale" element={<PublicSale />} /> */}
+          <Route path="public-sale" element={<PublicSale />} />
           <Route path="community-sale" element={<CommunitySale />} />
         </Routes>
       </Suspense>
