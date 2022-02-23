c9951667b6e3fd9644d3e5ef51c0726e00b8cbad
Frock <info@fractionalrocket.money>
2022-02-23 18:16:01 +0800

    scripts for exclude reward and exclude fees

--- packages/solidity/scripts/setExcludedFromReward.ts
+++ packages/solidity/scripts/setExcludedFromReward.ts
@@ -6,18 +6,26 @@ import { DividenDistributorProxy, DividenDistributorV1 } from '@project/contract
  */
 async function main() { 
 
-    var args = process.argv.slice(2);
-    const address = args[1];
-    
     // Acccounts
     const deployer = await ethers.getNamedSigner('deployer');
+    const communityOffering = await ethers.getContract<CommunityOffering>(`CommunityOffering`)
+    const fairLaunch = await ethers.getContract<FairPriceLaunch>(`FairPriceLaunch`)
     // Contracts
     const dividenDistributorProxy = await ethers.getContract<DividenDistributorProxy>('DividenDistributorProxy');
     const dividenDistributor = (await ethers.getContract<DividenDistributorV1>('DividenDistributorV1')).attach(dividenDistributorProxy.address);
     
     // Exclude From Reward
-    await dividenDistributor.connect(deployer).excludedFromReward(address, true);
-    
+    await dividenDistributor.connect(deployer).excludedFromReward(communityOffering.address, true);
+    console.log("communityOffering contract excluded from rewards");
+    await dividenDistributor.connect(deployer).excludedFromReward(fairLaunch.address, true);
+    console.log("fairLaunch contract excluded from rewards");
+    await dividenDistributor.connect(deployer).excludedFromReward('0x3e3C787744449fbe4fC275d48d8adDd642c482ae', true); //todo DividenDistributor??
+    console.log("Dividend Distributor excluded from rewards");
+    //await dividenDistributor.connect(deployer).excludedFromReward(.address, true); //todo LP
+    //todo console.log("LP excluded from rewards");
+
+
+
 }
 
 main()
