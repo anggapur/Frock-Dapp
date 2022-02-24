413d9138a11384cce2d6727e138084f818e00a5c
Angga Pur <anggapurdev@gmail.com>
2022-02-24 19:47:07 +0800

    Update Defender, Add Condition

--- packages/defender/src/index.js
+++ packages/defender/src/index.js
@@ -5,16 +5,34 @@ const {
 } = require("defender-relay-client/lib/ethers");
 const { address: dividenDistributorAddress } = require("./abi/DividenDistributorProxy.json");
 const { abi: dividenDistributorAbi } = require("./abi/DividenDistributorV1.json");
+const { address: frockAddress } = require("./abi/FrockProxy.json");
+const { abi: frockAbi } = require("./abi/FrockTokenV1.json");
 
 async function swapAndShareReward(payload, signer) {
   console.log("Start Swap FROCK & Share Reward");
+  // Contract
+  const frock = new ethers.Contract(frockAddress, frockAbi, signer);
   const dividenDistributor = new ethers.Contract(dividenDistributorAddress, dividenDistributorAbi, signer);
-  const tx = await dividenDistributor.connect(signer).swapAndShareReward();
-  console.log("End Swap FROCK & Share Reward");
-  return {
-    hash: tx.hash,
-    transactionId: tx.transactionId,
+  // Check Balance & Minimum Balance  
+  const balanceOfDividenDistributor = await frock.connect(signer).balanceOf("0x3e3C787744449fbe4fC275d48d8adDd642c482ae");
+  const minimumFrockToSwap = await dividenDistributor.connect(signer).minimumFrockToSwap();  
+
+  if(balanceOfDividenDistributor >= minimumFrockToSwap) {
+    console.log("Reach Minimum Requirement")
+    const tx = await dividenDistributor.connect(signer).swapAndShareReward();
+    return {
+      hash: tx.hash,
+      transactionId: tx.transactionId,
+    }
+  } else {
+    console.log("Not Reaching Minimum Requirement")
+    return {
+      hash: 0,
+      transactionId:0,
+    }
   }
+      
+  
 }
 
 // Entrypoint for the Autotask
