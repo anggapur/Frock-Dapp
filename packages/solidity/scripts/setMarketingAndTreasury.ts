92013796f0da3f2bdaceea95a5bf945722161e00
Frock <info@fractionalrocket.money>
2022-03-09 18:52:58 +0800

    new treasury contract default in setmarketingandtreasury script

--- packages/solidity/scripts/setMarketingAndTreasury.ts
+++ packages/solidity/scripts/setMarketingAndTreasury.ts
@@ -5,15 +5,15 @@ import { Vault, FrockProxy, FrockTokenV1} from '@project/contracts/typechain/gen
  * @dev to run this function :  yarn solidity run-local setMarketingAndTreasury.ts
  */
 async function main() {
-    console.log("Lock Token to Vault")
+    console.log("Set Marketing and/or Treasury contracts")
 
     // Define Contract
     const frockProxy = (await ethers.getContract<FrockProxy>('FrockProxy'))
     const frock = (await ethers.getContract<FrockTokenV1>('FrockTokenV1')).attach(frockProxy.address)         
     const deployer = await ethers.getNamedSigner('deployer'); // Need to change
-    const treasury = await ethers.getNamedSigner('treasury'); // Need to change
+    const treasuryContract = await ethers.getNamedSigner('treasuryContract'); // Need to change
     const marketing = await ethers.getNamedSigner('marketing'); // Need to change
-    await frock.connect(deployer).setTreasury(treasury.address);
+    await frock.connect(deployer).setTreasury(treasuryContract.address);
     await frock.connect(deployer).setMarketing(marketing.address);
 }
 
